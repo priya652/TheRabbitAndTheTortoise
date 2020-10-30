ec42b9b62d6dd4523e5b6f326b43db627d1fd822
priya kumari <priya.kumari_cs19@gla.ac.in>
2020-10-30 20:48:58 +0530

    using switch in the Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -3,6 +3,36 @@ import  javax.swing.*;
 public class Tortoise {
  private static final int MILESTONE=5;
  public void run() {
-
+  for (int index = 0; index < MILESTONES; index++) {
+   switch (index) {
+    case 0:
+     System.out.println("The Tortoise has started from the start line!");
+     break;
+    case 1:
+     System.out.println("The Tortoise has reached the cool River!");
+     break;
+    case 2:
+     System.out.println("The Tortoise has reached the Mountain Hill!");
+     break;
+    case 3:
+     System.out.println("The Tortoise has reached the big oak tree!");
+     break;
+    case 4:
+     System.out.println("The Tortoise has reached the finish line!");
+     break;
+   }
+   try {
+    Thread.sleep(3000L);
+   } catch (InterruptedException e) {
+    System.err.println("The tortoise has lost the path!");
+   }
+  }
+  JOptionPane.showConfirmDialog(
+          null,
+          "The Tortoise has completed the race!",
+          "Tortoise",
+          JOptionPane.OK_CANCEL_OPTION
+  );
+ }
  }
 }
